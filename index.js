dbecd368a257894e1409556b990e77b8cf93484d
Minh Vu <minhd_vu@yahoo.com>
2018-01-24 00:59:20 -0500

    added semiworking login feature (not really)

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
+var favicon = require('serve-favicon');
+var path = require('path');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+
+app.use(favicon(__dirname + '/favicon.ico'));
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
