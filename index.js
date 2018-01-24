f8b9600320264332983393d3981c51e46dabdc3d
Minh Vu <minhd_vu@yahoo.com>
2018-01-24 12:47:16 -0500

    Made style work with express

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+app.use(express.static('public'));
+
 app.use(favicon(__dirname + '/favicon.ico'));
 
 app.get('/', function(req, res) {
