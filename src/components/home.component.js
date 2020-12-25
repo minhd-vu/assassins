aa67a661c9737d5726aba9eacf0d69a90a89c19c
Minh Vu <minhd_vu@yahoo.com>
2020-12-25 02:01:38 -0500

    removed unused

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Party from "./party.component"
 import PartyForm from "./party.form.component"
 import { UserContext } from "../contexts/user.context";
@@ -6,7 +6,7 @@ import { useContext } from "react";
 
 export default function Home() {
     const user = useContext(UserContext);
-    
+
     return (
         user.partyCode ? <Party /> : <PartyForm />
     );
