be27e00300cba6e248889219a8f32cecc9e3a935
Minh Vu <minhd_vu@yahoo.com>
2020-12-25 15:41:39 -0500

    remove party if it has no more players

--- server/routes/party/leave.js
+++ server/routes/party/leave.js
@@ -5,6 +5,7 @@ const User = require("../../models/user.model");
 
 router.route("/").get(isLoggedIn, async function (req, res) {
     await Party.updateOne({ _id: req.user.party }, { $pullAll: { players: [req.user._id] } });
+    await Party.remove({ players: { $exists: true, $size: 0 } });
     req.user.party = null;
     await req.user.save();
     res.status(200).send();
