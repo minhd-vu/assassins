2c8b5c21770fa1292c896a522666f36064148622
Minh Vu <minhd_vu@yahoo.com>
2020-12-25 18:23:31 -0500

    update leave

--- server/routes/party/leave.js
+++ server/routes/party/leave.js
@@ -4,7 +4,7 @@ const Party = require("../../models/party.model");
 
 router.route("/").get(isLoggedIn, async function (req, res) {
     await Party.updateOne({ _id: req.user.party }, { $pullAll: { players: [req.user._id] } });
-    await Party.remove({ players: { $exists: true, $size: 0 } });
+    await Party.deleteOne({ players: { $exists: true, $size: 0 } });
     req.user.party = null;
     await req.user.save();
     res.status(200).send();
