bd231561edc32600e86da72beafaca31d78f0c1b
Minh Vu <minhd_vu@yahoo.com>
2020-12-25 15:14:00 -0500

    fixed leave problem

--- server/routes/party/leave.js
+++ server/routes/party/leave.js
@@ -4,7 +4,7 @@ const Party = require("../../models/party.model");
 const User = require("../../models/user.model");
 
 router.route("/").get(isLoggedIn, async function (req, res) {
-    Party.updateOne({ _id: req.user.party }, { $pullAll: { players: [req.user._id] } });
+    await Party.updateOne({ _id: req.user.party }, { $pullAll: { players: [req.user._id] } });
     req.user.party = null;
     await req.user.save();
     res.status(200).send();
