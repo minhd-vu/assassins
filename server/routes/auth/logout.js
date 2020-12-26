49942a46c371dde42950a726d81a571c0630d3db
Minh Vu <minhd_vu@yahoo.com>
2020-12-26 13:48:09 -0500

    clear cookie session on logout

--- server/routes/auth/logout.js
+++ server/routes/auth/logout.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 router.route("/").get(function (req, res) {
     console.log(req.user);
     req.logout();
+    req.session = null;
     res.status(200).send();
 });
 
