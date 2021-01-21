858e3fb03481eb5dcd868a3976a8ba3557b2f25e
Minh Vu <minhd_vu@yahoo.com>
2021-01-21 08:20:55 -0500

    update async deny

--- server/routes/assassinate/deny.js
+++ server/routes/assassinate/deny.js
@@ -4,7 +4,7 @@ const isLoggedIn = require("../../helpers/isLoggedIn");
 router.route("/").get(isLoggedIn, async function (req, res) {
     if (req.user.isPending) {
         req.user.isPending = false;
-        req.user.save();
+        await req.user.save();
     }
     res.status(200).send();
 });
