591b74eb44ad6f64636887c9898467851499dc99
Minh Vu <minhd_vu@yahoo.com>
2021-01-21 08:32:56 -0500

    clear target and increment deaths on confirm

--- server/routes/assassinate/confirm.js
+++ server/routes/assassinate/confirm.js
@@ -1,11 +1,18 @@
 const router = require("express").Router();
 const isLoggedIn = require("../../helpers/isLoggedIn");
+const User = require("../../models/user.model");
 
 router.route("/").get(isLoggedIn, async function (req, res) {
     if (req.user.isPending) {
         req.user.isAlive = false;
         req.user.isPending = false;
+        req.user.target = null;
+        req.user.stats.deaths += 1;
         await req.user.save();
+
+        await User.findOne({ "target": req.user._id }, async function (err, user) {
+            
+        });
     }
     res.status(200).send();
 });
