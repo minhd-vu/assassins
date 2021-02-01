f9a3718e2915eb0e47b7765d61e434a920378a83
Minh Vu <minhd_vu@yahoo.com>
2021-01-31 23:50:19 -0500

    found bug not reseting player alive #15

--- server/routes/party/start.js
+++ server/routes/party/start.js
@@ -14,6 +14,7 @@ router.route("/").post(isLoggedIn, function (req, res) {
         party.gameMode = req.body.gameMode;
         party.showPlayers = req.body.showPlayers;
         party.isStarted = true;
+        party.winner = null;
 
         await party.save();
         await party.execPopulate("players");
@@ -22,7 +23,7 @@ router.route("/").post(isLoggedIn, function (req, res) {
          * Assign a new target for each player.
          * Reset the isAlive and isPending variables so they don't transfer.
          */
-        const players = shuffle(party.players.filter(player => player.isAlive));
+        const players = shuffle(party.players);
         await players.forEach((player, i) => {
             if (i === players.length - 1) {
                 player.target = players[0]._id;
