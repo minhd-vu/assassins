7a5e53fa13f32261b25798c36309515e26b35a55
Minh Vu <minhd_vu@yahoo.com>
2021-05-06 01:13:45 -0400

    update leave to fix null error

--- server/routes/party/leave.js
+++ server/routes/party/leave.js
@@ -8,17 +8,17 @@ router.route("/").get(isLoggedIn, async function (req, res) {
     await Party.updateOne({ _id: req.user.party }, { $pullAll: { players: [req.user._id] } });
     await Party.deleteOne({ players: { $exists: true, $size: 0 } });
 
-    if (req.user.isAdmin) {
-        await req.user.execPopulate("party");
-        await req.user.party.execPopulate("players");
-        const player = req.user.party.players[Math.floor(Math.random() * req.user.party.players.length)];
-        player.isAdmin = true;
-        await player.save();
-    }
-
     await req.user.execPopulate("party");
 
     if (req.user.party) {
+        // Set a random player to be the new party admin.
+        if (req.user.isAdmin) {
+            await req.user.party.execPopulate("players");
+            const player = req.user.party.players[Math.floor(Math.random() * req.user.party.players.length)];
+            player.isAdmin = true;
+            await player.save();
+        }
+
         const target = req.user.target;
 
         await User.findOne({ "target": req.user._id }, async function (err, user) {
