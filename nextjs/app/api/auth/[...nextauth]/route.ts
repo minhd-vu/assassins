e290ca61a3e4fd93095727b405d703570e6f31dd
minhd-vu <minhd_vu@yahoo.com>
2024-02-08 17:41:42 -0500

    add callbacks

--- nextjs/app/api/auth/[...nextauth]/route.ts
+++ nextjs/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
+import dbConnect from "@/lib/dbConnect";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import User from "@/models/User";
 
 const options = {
   secret: process.env.NEXTAUTH_SECRET,
@@ -14,6 +16,36 @@ const options = {
       clientSecret: process.env.GOOGLE_SECRET!,
     }),
   ],
+  callbacks: {
+    async signIn({ user }: { user: any }) {
+      await dbConnect();
+
+      try {
+        const oldUser = await User.findOne({ email: user.email });
+
+        if (!oldUser) {
+          const newUser = new User({
+            email: user.email,
+            name: user.name,
+          });
+          await newUser.save();
+        }
+
+        return true;
+      } catch (err) {
+        console.log("Error saving user", err);
+        return false;
+      }
+    },
+    async session({ session }: { session: any }) {
+      await dbConnect();
+
+      const user = await User.findOne({ email: session.user.email });
+      session.user = user;
+
+      return session;
+    },
+  },
 };
 
 const handler = NextAuth(options);
