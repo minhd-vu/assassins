56dac221ae7199efc975703f127f3d8aeb91a8df
minhd-vu <minhd_vu@yahoo.com>
2024-02-08 16:44:52 -0500

    add google provider

--- nextjs/app/api/auth/[...nextauth]/route.ts
+++ nextjs/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
 const options = {
   secret: process.env.NEXTAUTH_SECRET,
@@ -8,9 +9,12 @@ const options = {
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
   ],
 };
 
 const handler = NextAuth(options);
-
 export { handler as GET, handler as POST };
