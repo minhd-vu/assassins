a8db8eee6e0f493ea5bafe98f6082cbb62333c4a
minhd-vu <minhd_vu@yahoo.com>
2024-02-13 01:02:41 -0500

    move leaderboard

--- nextjs/app/leaderboard/page.tsx
+++ nextjs/app/leaderboard/page.tsx
@@ -1,7 +1,28 @@
-import { getLeaderboardUsers } from "../api/leaderboard/route";
+import prisma from "@/lib/prisma";
+
+export async function getLeaderboardUsers() {
+  return await prisma.user.findMany({
+    select: {
+      name: true,
+      kills: true,
+      deaths: true,
+      wins: true,
+    },
+    orderBy: [
+      {
+        wins: "asc",
+      },
+      {
+        kills: "asc",
+      },
+      {
+        deaths: "desc",
+      },
+    ],
+  });
+}
 
 export default async function Leaderboard() {
   const users = await getLeaderboardUsers();
-
   return <div>{JSON.stringify(users)}</div>;
 }
