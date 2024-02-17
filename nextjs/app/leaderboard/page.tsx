c3b87b8f2d65ba033545d718e084d2d080d4ef93
minhd-vu <minhd_vu@yahoo.com>
2024-02-17 01:28:58 -0500

    add leaderboard table

--- nextjs/app/leaderboard/page.tsx
+++ nextjs/app/leaderboard/page.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/Navbar";
 import prisma from "@/lib/prisma";
 
 export async function getLeaderboardUsers() {
@@ -24,5 +25,48 @@ export async function getLeaderboardUsers() {
 
 export default async function Leaderboard() {
   const users = await getLeaderboardUsers();
-  return <div>{JSON.stringify(users)}</div>;
+
+  const rows = users.map((user) => (
+    <tr
+      key={user.name}
+      className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+    >
+      <th
+        scope="row"
+        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+      >
+        {user.name}
+      </th>
+      <td className="px-6 py-4">{user.kills}</td>
+      <td className="px-6 py-4">{user.deaths}</td>
+      <td className="px-6 py-4">{user.wins}</td>
+    </tr>
+  ));
+
+  return (
+    <>
+      <Navbar />
+      <div className="relative overflow-x-auto">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Kills
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Deaths
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Wins
+              </th>
+            </tr>
+          </thead>
+          <tbody>{rows}</tbody>
+        </table>
+      </div>
+    </>
+  );
 }
