090d14e0a1f7a4957cbf3965fbbadcd3d7a40cb4
minhd-vu <minhd_vu@yahoo.com>
2024-02-22 21:54:33 -0500

    add kill target

---
+++ components/KillTarget.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+export default function KillTarget() {
+  const router = useRouter();
+
+  async function killTarget() {
+    const res = await fetch("/api/kill", {
+      method: "POST",
+    });
+
+    if (!res.ok) {
+      throw new Error(await res.json());
+    }
+
+    console.log(await res.json());
+
+    router.refresh();
+  }
+
+  return (
+    <button
+      type="button"
+      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+      onClick={() => killTarget()}
+    >
+      Kill Target
+    </button>
+  );
+}
