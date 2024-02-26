aeff7fd80d7726464c2a670b54c921ef8dd75b4d
minhd-vu <minhd_vu@yahoo.com>
2024-02-26 01:43:27 -0500

    add profile to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,18 @@
+import { getServerSession } from "next-auth";
 import LoginButton from "./LoginButton";
 import ThemeController from "./ThemeController";
+import prisma from "@/lib/prisma";
+import { User } from "@prisma/client";
 
 export default async function Navbar() {
+  const session = await getServerSession();
+  let user: User | null = null;
+  if (session?.user?.email) {
+    user = await prisma.user.findUnique({
+      where: { email: session.user.email },
+    });
+  }
+
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
@@ -11,6 +22,11 @@ export default async function Navbar() {
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-2">
+          {user && (
+            <li>
+              <a href={`/user/${user.id}`}>Profile</a>
+            </li>
+          )}
           <li>
             <a href="/leaderboard">Leaderboard</a>
           </li>
