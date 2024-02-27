d073b5a2577783e9006a7858f8704e4eed97d3c9
minhd-vu <minhd_vu@yahoo.com>
2024-02-27 02:01:53 -0500

    update headers

--- app/help/page.tsx
+++ app/help/page.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 export default function Help() {
   return (
     <div className="prose m-6">
-      <h2 id="how-to-play">How to Play</h2>
+      <h1 id="how-to-play">How to Play</h1>
       <p>
         Assassins is a live action game that can be played almost anywhere, but
         best done in semi-crowded environments. The goal of the game is to be
         the last remaining person alive and to assassinate whomever you are
         assigned.
       </p>
-      <h4 id="create-a-user">Create a User</h4>
+      <h2 id="login">Login</h2>
       <p>
         Before doing anything, you should create a user. It will keep track of
         your statistics as well as identifying who you are in a party. You can
@@ -22,41 +22,41 @@ export default function Help() {
         If you already have a user, you can login by heading to the login link
         in the navbar.
       </p>
-      <h4 id="party-system">Party System</h4>
+      <h2 id="party-system">Party System</h2>
       <p>
         Once you have logged in you have the option of joining or creating a
         party. A party is an instance of the assassins game; it is how you
         connect with your friends.
       </p>
-      <h5 id="join-a-party">Join a Party</h5>
+      <h3 id="join-a-party">Join a Party</h3>
       <p>
         To join a party, type in the party code in the input field on the{" "}
         <Link href="/">home page</Link> then click the{" "}
         <strong>Join Party</strong> button. Once a party has started you can no
         longer join it.
       </p>
-      <h5 id="create-a-party">Create a Party</h5>
+      <h3 id="create-a-party">Create a Party</h3>
       <p>
         To create a party, click the <strong>Create Party</strong> button on the
         home page. Once you have created it, the app will display the party code
         in bold. Give this code to your friends so that they can join.
       </p>
-      <h5 id="leave-a-party">Leave a Party</h5>
+      <h3 id="leave-a-party">Leave a Party</h3>
       <p>
         Once you join a party, you have the option of leaving it. You can leave
         it anytime, and the app will handle the reshuffling of players.
       </p>
-      <h5 id="start-a-party">Start a Party</h5>
+      <h3 id="start-a-party">Start a Party</h3>
       <p>
         Only the party leader can start the party. Once started everyone is
         assigned a target.
       </p>
-      <h4 id="eliminate-targets">Eliminate Targets</h4>
+      <h2 id="eliminate-targets">Eliminate Targets</h2>
       <p>
         You will be assigned a target to assassinate, hence the name{" "}
         <em>assassins</em>. Once you assassinated your target, either by means
         of dart blasters, tag, projectile launchers, or any other method, click
-        the <strong>Assassinate</strong> button.
+        the <strong>Kill Target</strong> button.
       </p>
       <p>
         A notification will be send to the target just eliminated, the target
@@ -65,13 +65,13 @@ export default function Help() {
         accidental clicks. If the attempt is successful, you will be assigned a
         new target depending on the game mode.
       </p>
-      <h4 id="game-modes">Game Modes</h4>
-      <h5 id="classic">Classic</h5>
+      <h2 id="game-modes">Game Modes</h2>
+      <h3 id="classic">Classic</h3>
       <p>
         When you assassinate a target, your next target will be the target your
         target had.
       </p>
-      <h5 id="shuffle">Shuffle</h5>
+      <h3 id="shuffle">Shuffle</h3>
       <p>
         When a player assassinates a target, all players will receive a new
         target from the remaining players alive.
