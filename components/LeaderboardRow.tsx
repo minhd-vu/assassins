cda9845b2e54ddf5aa0247722e60c5c74bd6b190
minhd-vu <minhd_vu@yahoo.com>
2024-02-28 18:54:46 -0500

    add link to leaderboard row for seo

--- components/LeaderboardRow.tsx
+++ components/LeaderboardRow.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { LeaderboardUser } from "@/lib/user";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export default function LeaderboardRow({
@@ -19,7 +20,11 @@ export default function LeaderboardRow({
   return (
     <tr className="hover" onClick={() => onClick()}>
       <td>{rank}</td>
-      <td>{user.name}</td>
+      <td>
+        <Link className="link link-primary" href={`/user/${user.name}`}>
+          {user.name}
+        </Link>
+      </td>
       <td>{user.kills}</td>
       <td>{user.deaths}</td>
       <td>{user.wins}</td>
