ef06ac4e36c56a3ee3e6071e16212d4a94280413
minhd-vu <minhd_vu@yahoo.com>
2024-02-28 20:46:36 -0500

    update help

--- app/help/page.tsx
+++ app/help/page.tsx
@@ -7,77 +7,79 @@ export default function Help() {
         <h1 id="how-to-play">How to Play</h1>
         <h2 id="about">About</h2>
         <p>
-          Assassins is a live action game that can be played almost anywhere,
-          but best done in semi-crowded environments. The goal of the game is to
-          be the last remaining person alive and to assassinate whomever you are
-          assigned.
+          Welcome to Assassins, a thrilling live-action game designed for play
+          in semi-crowded environments. The ultimate objective is to emerge as
+          the last surviving player while successfully assassinating your
+          assigned targets. Here's a step-by-step guide to get you started:
         </p>
         <h2 id="login">Login</h2>
         <p>
-          Before doing anything, you should create a user. It will keep track of
-          your statistics as well as identifying who you are in a party. You can
-          register yourself by heading to the register link in the navbar. Once
-          you create a user, you will no longer be able to change the username
-          or password.
-        </p>
-        <p>
-          If you already have a user, you can login by heading to the login link
-          in the navbar.
+          Before diving into the action, create a user account to track your
+          statistics and establish your identity within the game. Click on the{" "}
+          <Link href="/api/auth/signin">Login</Link> button in the navbar to
+          regsister an account. Note that once your user is created, changes to
+          the username are no longer possible. If you already have a user,
+          simply sign in by clicking on the login button.
         </p>
         <h2 id="party-system">Party System</h2>
         <p>
-          Once you have logged in you have the option of joining or creating a
-          party. A party is an instance of the assassins game; it is how you
-          connect with your friends.
+          Upon logging in, you have the option to either join an existing party
+          or create a new one. A party serves as an instance of the Assassins
+          game and is how you connect with friends.
         </p>
         <h3 id="join-a-party">Join a Party</h3>
         <p>
-          To join a party, type in the party code in the input field on the{" "}
-          <Link href="/">home page</Link> then click the{" "}
-          <strong>Join Party</strong> button. Once a party has started you can
-          no longer join it.
+          Type the party code into the input field on the{" "}
+          <Link href="/">home page</Link> and click the{" "}
+          <strong>Join Party</strong> button. Once a party has started, joining
+          is no longer possible.
         </p>
         <h3 id="create-a-party">Create a Party</h3>
         <p>
-          To create a party, click the <strong>Create Party</strong> button on
-          the home page. Once you have created it, the app will display the
-          party code in bold. Give this code to your friends so that they can
-          join.
+          Click the <strong>Create Party</strong> button on the{" "}
+          <Link href="/">home page</Link>. The app will display the
+          six-character party code at the top; share this code with your friends
+          for them to join.
         </p>
         <h3 id="leave-a-party">Leave a Party</h3>
         <p>
-          Once you join a party, you have the option of leaving it. You can
-          leave it anytime, and the app will handle the reshuffling of players.
+          You can leave a party at any time, and the app will handle player
+          reshuffling.
         </p>
         <h3 id="start-a-party">Start a Party</h3>
         <p>
-          Only the party leader can start the party. Once started everyone is
-          assigned a target.
+          Only the party leader can initiate the party. Once started, everyone
+          is assigned a target.
         </p>
         <h2 id="eliminate-targets">Eliminate Targets</h2>
         <p>
-          You will be assigned a target to assassinate, hence the name{" "}
-          <em>assassins</em>. Once you assassinated your target, either by means
-          of dart blasters, tag, projectile launchers, or any other method,
-          click the <strong>Kill Target</strong> button.
+          Your objective is to assassinate the assigned targets, utilizing
+          methods such as dart blasters, tag, projectile launchers, or any
+          approved means. Click the <strong>Kill Target</strong> button after
+          eliminating your target.
         </p>
         <p>
-          A notification will be send to the target just eliminated, the target
-          will have option of confirming or denying the assassination attempt.
-          If the attempt is denied, the game continues; this is in place to
-          prevent accidental clicks. If the attempt is successful, you will be
-          assigned a new target depending on the game mode.
+          A notification is sent to the eliminated target, who can choose to
+          confirm or deny the assassination attempt. If denied, the game
+          continues to avoid accidental clicks. If successful, you are assigned
+          a new target based on the selected game mode.
         </p>
         <h2 id="game-modes">Game Modes</h2>
+        <p>Choose from different game modes to spice up the gameplay:</p>
         <h3 id="classic">Classic</h3>
         <p>
-          When you assassinate a target, your next target will be the target
-          your target had.
+          After assassinating a target, your next target will be the one your
+          previous target had.
         </p>
         <h3 id="shuffle">Shuffle</h3>
         <p>
-          When a player assassinates a target, all players will receive a new
-          target from the remaining players alive.
+          When a player eliminates a target, all players receive new targets
+          from the remaining participants.
+        </p>
+        <h2>Good Luck!</h2>
+        <p className="italic">
+          Embark on the adrenaline-pumping journey of Assassins, where strategy
+          and stealth determine your survival and success!
         </p>
       </div>
     </div>
