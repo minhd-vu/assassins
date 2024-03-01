d7938c663050d396651dc2c6afe19077cf22afaa
minhd-vu <minhd_vu@yahoo.com>
2024-03-01 15:44:21 -0500

    remove email from queries

--- lib/user.ts
+++ lib/user.ts
@@ -7,17 +7,45 @@ export type UserBody = {
   name?: string;
 };
 
+type BooleanObject<T> = {
+  [K in keyof T]?: boolean;
+};
+
+export const exclude = <T>(
+  fields: T,
+  excludes: (keyof T)[],
+): BooleanObject<T> => {
+  const object: BooleanObject<T> = {};
+  const keys = Object.keys(fields!).filter(
+    (key) => !excludes.includes(key as keyof T),
+  ) as (keyof T)[];
+
+  for (let key of keys) {
+    object[key] = true;
+  }
+
+  return object;
+};
+
+const select = exclude(prisma.user.fields, ["email"]);
+
 export async function getUser(email: string) {
   return await prisma.user.findUnique({
     where: {
       email,
     },
     include: {
-      target: true,
+      target: {
+        select,
+      },
       party: {
         include: {
-          players: true,
-          winner: true,
+          players: {
+            select,
+          },
+          winner: {
+            select,
+          },
         },
       },
     },
