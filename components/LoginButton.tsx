8b16e90718d47e555609ec9b3eecce862a1b861e
minhd-vu <minhd_vu@yahoo.com>
2024-03-02 02:11:32 -0500

    fix navbar login button

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { useSession, signIn, signOut } from "next-auth/react";
+import { useContext } from "react";
+import { UserContext } from "./UserProvider";
 
 export default function LoginButton({ className }: { className?: string }) {
   const { data: session } = useSession();
+  const { user } = useContext(UserContext);
 
-  if (session) {
+  if (session || user) {
     return (
       <button className={className} onClick={() => signOut()}>
         Logout
