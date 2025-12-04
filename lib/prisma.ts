eb8a18cdf698bf3764c0dca3e59e1bb5968a74e6
minhd-vu <minhd_vu@yahoo.com>
2025-12-04 17:13:16 -0500

    fix: prisma dev dependency

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
-  return new PrismaClient({
-    datasourceUrl: process.env.MONGODB_URI,
-  });
+  return new PrismaClient();
 };
 
 declare global {
--- prisma/prisma.config.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaConfig } from "@prisma/client";
-
-export default {
-  datasources: {
-    db: {
-      url: process.env.MONGODB_URI,
-    },
-  },
-} satisfies PrismaConfig;
